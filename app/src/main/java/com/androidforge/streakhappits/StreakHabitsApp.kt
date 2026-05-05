b64c36426bb761c7aac318e5bc1d7444ce81d3b1
omkar9 <omkar946@gmail.com>
2026-05-05 11:05:20 +0530

    fix: update StreakHabitsApp.kt

--- app/src/main/java/com/androidforge/streakhappits/StreakHabitsApp.kt
+++ app/src/main/java/com/androidforge/streakhappits/StreakHabitsApp.kt
@@ -1,26 +1,7 @@
 package com.androidforge.streakhappits
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
-import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class StreakHabitsApp : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override fun onCreate() {
-        super.onCreate()
-        // Initialize the Mobile Ads SDK.
-        MobileAds.initialize(this) {}
-    }
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-}
+class StreakHabitsApp : Application()
